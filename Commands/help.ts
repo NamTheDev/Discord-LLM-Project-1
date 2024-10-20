171729bd37cead7cfd1a10faf03d38904fa78b87
KainTheDev <thica.doi@gmail.com>
2024-10-21 05:34:13 +0700

    refactor: Update import syntax in help command

--- Commands/help.ts
+++ Commands/help.ts
@@ -1,18 +1,18 @@
 import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { outputEmbed } from '../utils/outputEmbed';
-import commands from '../index';
+import { commands } from '../index';
 
 const helpCommand = new SlashCommandBuilder()
-  .setName('help')
-  .setDescription('Displays help information');
+    .setName('help')
+    .setDescription('Displays help information');
 
 const commandsList = commands.map((command) => `\`/${command.data.name}\``);
 
 const output = commandsList.join(', ');
 
 export default {
-  data: helpCommand,
-  execute: async (interaction: CommandInteraction) => {
-    const helpEmbed = new outputEmbed(__filename, output);
-  },
+    data: helpCommand,
+    execute: async (interaction: CommandInteraction) => {
+        const helpEmbed = new outputEmbed(__filename, output);
+    },
 };

056f2e91242cf5c93e312843a61386614b215a3e
KainTheDev <thica.doi@gmail.com>
2024-10-20 23:33:46 +0700

    refactor: Simplify and reorganize code structure

--- index.ts
+++ index.ts
@@ -3,50 +3,36 @@ import eventsHandler from './Handlers/EventsHandler';
 import commandsHandler from './Handlers/CommandsHandler';
 import interactionsHandler from './Handlers/InteractionsHandler';
 
-// Create a new Discord client instance with the specified intents and partials
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], partials: [Partials.Channel] });
 
-// Define the Command interface with data, autocomplete, and execute functions
 export interface Command {
-    // Command data
     data: {
         name: string;
         description: string;
     };
-    // Autocomplete function
     autocomplete: (interaction: import('discord.js').AutocompleteInteraction, client?: Client) => Promise<void>;
-    // Execute function
     execute: (interaction: import('discord.js').Interaction, client?: Client) => Promise<void>;
 }
-// Define a custom collection class for commands
+
 class CommandsCollection extends Collection<string, Command> {
-    // Get command by name
     get(name: string) {
         return super.find((command) => command.data.name === name);
     }
 }
 
-// Define a custom collection class for interactions
 class InteractionsCollection extends Collection<string, (interaction: ButtonInteraction | AnySelectMenuInteraction, client?: Client) => Promise<void>> {
-    // Get interaction by name
     get(name: string) {
         return this.find((value, key) => key === name);
     }
 }
 
-// Create instances of the CommandsCollection and InteractionsCollection
 const commands = new CommandsCollection();
-// Initialize the commands collection
 const interactions = new InteractionsCollection();
-// Initialize the interactions collection
 
 export { commands, interactions };
 
-// Initialize event handlers
 eventsHandler(client);
-// Initialize command handlers
 commandsHandler();
-// Initialize interaction handlers
 interactionsHandler();
 
 client.login(process.env.TOKEN);

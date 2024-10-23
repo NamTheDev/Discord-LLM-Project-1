4a9cac0e86cca41ccd18c5c49856e08a29f3f11a
NamTheDev <thica.doi@gmail.com>
2024-10-23 19:47:55 +0700

    feat: Integrate Groq SDK for chat completion and webhook handling

--- index.ts
+++ index.ts
@@ -1,38 +1,122 @@
-import { ButtonInteraction, Client, Collection, GatewayIntentBits, Partials, type AnySelectMenuInteraction } from 'discord.js';
-import eventsHandler from './Handlers/EventsHandler';
-import commandsHandler from './Handlers/CommandsHandler';
-import interactionsHandler from './Handlers/InteractionsHandler';
+import { ChannelType, Client, GatewayIntentBits, Partials } from 'discord.js';
+import Groq from 'groq-sdk';
+import type { RequestOptions } from 'groq-sdk/core.mjs';
+import type { ChatCompletionCreateParamsNonStreaming } from 'groq-sdk/resources/chat/completions.mjs';
+import fetch from 'node-fetch';
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], partials: [Partials.Channel] });
 
-export interface Command {
-    data: {
-        name: string;
-        description: string;
-    };
-    autocomplete: (interaction: import('discord.js').AutocompleteInteraction, client?: Client) => Promise<void>;
-    execute: (interaction: import('discord.js').Interaction, client?: Client) => Promise<void>;
+const groq_client = new Groq({
+    apiKey: Bun.env.GROQ_API_KEY
+})
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageTyping
+    ], partials: [Partials.Channel]
+});
+
+let process_running = false;
+
+async function chat(body: ChatCompletionCreateParamsNonStreaming, options: RequestOptions = {}) {
+    return groq_client.chat.completions.create(body, options)
 }
 
-class CommandsCollection extends Collection<string, Command> {
-    get(name: string) {
-        return super.find((command) => command.data.name === name);
+client.on('messageCreate', async (message) => {
+    const mention = message.mentions.users.first()
+    const client_ID = Bun.env.CLIENT_ID
+
+    if(!mention || mention.id !== client_ID) return;
+
+    if (process_running) return;
+
+    const webhookURL = `http://${Bun.env.N8N_HOST_IP}:5678/webhook/`
+
+    await message.channel.sendTyping()
+
+    const JSON_schema = {
+        "type": "object",
+        "properties": {
+            "prompt_type": {
+                "type": "string",
+                "description": "The type of prompt. Either: chat - conversational messages, search - browsing the internet or transcript - generate a transcript of a youtube video.",
+                "enum": [
+                    "chat",
+                    "search",
+                    "transcript"
+                ]
+            },
+            "chat_response": {
+                "type": "string",
+                "description": "The response for 'chat' prompt type."
+            },
+            "queries": {
+                "type": "string",
+                "description": "The queries to pass in when fetching webhooks. '?query' is for search and '?url=' is for transcript.",
+                "enum": [
+                    "?query=",
+                    "?url="
+                ]
+            }
+        },
+        "required": [
+            "prompt_type",
+            "queries"
+        ]
     }
-}
 
-class InteractionsCollection extends Collection<string, (interaction: ButtonInteraction | AnySelectMenuInteraction, client?: Client) => Promise<void>> {
-    get(name: string) {
-        return this.find((value, key) => key === name);
+
+    const chatCompletion = await chat({
+        response_format: {
+            type: 'json_object'
+        },
+        messages: [
+            {
+                role: 'user',
+                content: message.content
+            },
+            {
+                role: 'system',
+                content: `JSON schema: ${JSON.stringify(JSON_schema)}`
+            },
+            {
+                role: 'system',
+                content: 'Analyze user response and generate a respond with provided the JSON schema.'
+            }
+        ],
+        model: 'llama-3.2-90b-text-preview'
+    });
+    const { prompt_type, queries, chat_response } = chatCompletion.choices[0].message.content ? JSON.parse(chatCompletion.choices[0].message.content) : {};
+    
+    if (prompt_type === 'chat') {
+        await message.reply({
+            content: chat_response,
+            allowedMentions: {
+                repliedUser: false
+            }
+        });
+    } else {
+        const response = await fetch(webhookURL + prompt_type + queries);
+        const data = await response.json() as { output: string };
+        const { output } = data;
+
+        await message.reply({
+            embeds: [{
+                title: prompt_type,
+                description: output
+            }],
+            allowedMentions: {
+                repliedUser: false
+            }
+        });
     }
-}
 
-const commands = new CommandsCollection();
-const interactions = new InteractionsCollection();
+    process_running = false;
+});
 
-export { commands, interactions };
-
-eventsHandler(client);
-commandsHandler();
-interactionsHandler();
+client.on('ready', (client) => console.log(client.user.tag));
 
 client.login(process.env.TOKEN);
